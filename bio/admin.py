2e5aae8fd8b95baeddf6ebbc0a7a147d7ff490e1
alexaled <alexaled@gmail.com>
2011-07-15 14:56:27 +0300

    3_middleware_final

---
+++ bio/admin.py
@@ -0,0 +1,16 @@
+__author__ = 'alexaled'
+  
+import models
+from django.contrib import admin
+
+
+class ModelAdminHttpMidleware(admin.ModelAdmin):
+    fields = ( 'http_request', 'remote_addr',)
+    list_display = ( 'http_request', 'remote_addr',)
+
+
+    
+
+#register modfels in admin interface
+admin.site.register(models.MyBio)
+admin.site.register(models.HttpRequestSave, ModelAdminHttpMidleware)
