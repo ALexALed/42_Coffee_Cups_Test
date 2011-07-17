a0d23da02b70b14fe87546e8ba3b493357400e7b
alexaled <alexaled@gmail.com>
2011-07-17 18:52:43 +0300

    test_complete

--- Makefile
+++ Makefile
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-python manage.py test bio
-=======
 test: manage.py test bio
 	python manage.py test bio
->>>>>>> Ticket_1
+
--- urls.py
+++ urls.py
@@ -5,15 +5,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/',  include(admin.site.urls)),
-<<<<<<< HEAD
-    url(r'^my-bio/', include('Coffee_Cups_Test.bio.urls')),
-    url(r'^$',       include('Coffee_Cups_Test.bio.urls')),
+    url(r'^my-bio/', include('bio.urls')),
+    url(r'^$',       include('bio.urls')),
     #accounts
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 
-=======
-    url(r'^my-bio/', include('bio.urls')),
-    url(r'^$',       include('bio.urls')),
->>>>>>> Ticket_1
 )
