62abff0dafbce3e2038cf747d018b9fd0281581a
alexaled <alexaled@gmail.com>
2011-07-18 08:54:32 +0300

    test_done

--- urls.py
+++ urls.py
@@ -5,6 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/',  include(admin.site.urls)),
-    url(r'^my-bio/', include('Coffee_Cups_Test.bio.urls')),
-    url(r'^$',       include('Coffee_Cups_Test.bio.urls')),
+    url(r'^my-bio/', include('bio.urls')),
+    url(r'^$',       include('bio.urls')),
 )
