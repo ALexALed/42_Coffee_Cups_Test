1af79ea5e8b8c1e561416af32cdb0d9f77233f42
alexaled <alexaled@gmail.com>
2011-07-20 21:09:36 +0300

    test_complete

--- bio/models.py
+++ bio/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+from django.db.models.signals import post_init, post_save, post_delete
+import datetime
+
+def signals_init(sender, **kwargs):
+    save_signal(sender, 'init')
+
+def signals_save(sender, **kwargs):
+    save_signal(sender, 'save')
+
+def signals_delete(sender, **kwargs):
+    save_signal(sender, 'delete')
+
+
+def save_signal(sender, signal):
+    if sender.__name__ != 'DbSignals':
+        obj = DbSignals()
+        obj.model = sender.__name__
+        obj.signal = signal
+        obj.date = datetime.datetime.now()
+        obj.save()
+
+post_init.connect(signals_init, dispatch_uid='Coffee_Cups_test.bio')
+post_save.connect(signals_save, dispatch_uid='Coffee_Cups_test.bio')
+post_delete.connect(signals_delete, dispatch_uid='Coffee_Cups_test.bio')
+
 
 class MyBio(models.Model):
     '''
@@ -24,8 +49,10 @@ class HttpRequestSave(models.Model):
     http_request = models.CharField(max_length=300)
     remote_addr  = models.IPAddressField(blank=True)
 
-
-
-
-
-
+class DbSignals(models.Model):
+    """
+    table for signals saver
+    """
+    signal = models.CharField(max_length=50, blank=True)
+    model  = models.CharField(max_length=100, blank=True)
+    date   = models.DateTimeField(blank=True)
