4ab19c521164d265ca39ed8947e0eac03c7dce30
alexaled <alexaled@gmail.com>
2011-08-26 14:24:01 +0300

    ticket:19 fixed widget

--- bio/forms.py
+++ bio/forms.py
@@ -5,13 +5,16 @@ from django import forms
 from models import MyBio, HttpRequestSave
 
 
+class CalendarWidget(forms.TextInput):
+
+    class Media:
+        js = ('/media/js/jquery-1.5.1.min.js',
+              '/media/js/jquery.datapick.js')
+        css = {'all' : '/media/css/css/jquery-ui-1.8.14.custom.css'}
+
+
 class BioForm(forms.ModelForm):
-    birth_date = forms.DateField(('%Y-%m-%d',),
-                                    label='Birth Date', required=False,
-        widget=forms.DateTimeInput(format='%Y-%m-%d', attrs={
-            'class': 'input',
-            'readonly': 'readonly',
-            'size': '15'}))
+    birth_date = forms.DateField(widget=CalendarWidget)
 
     class Meta:
         model = MyBio
